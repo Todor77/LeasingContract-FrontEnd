579aa0ba4eee4bb57fe826333dde6046db4b2369
todorilievski <todor.ilievski@byteworx.de>
2022-10-09 21:36:47 +0200

    get brand and models as a select options

--- src/app/components/leasing-contract-details/leasing-contract-details.component.ts
+++ src/app/components/leasing-contract-details/leasing-contract-details.component.ts
@@ -3,6 +3,9 @@ import {LeasingContract} from "../../models/leasing-contract.model";
 import {LeasingContractService} from "../../services/leasing-contract.service";
 import {ActivatedRoute} from "@angular/router";
 import { Router } from '@angular/router';
+import {BrandService} from "../../services/brand.service";
+import {Brand} from "../../models/brand";
+import {Model} from "../../models/model";
 
 @Component({
   selector: 'app-leasing-contract-details',
@@ -14,26 +17,28 @@ export class LeasingContractDetailsComponent implements OnInit {
   message = '';
   leasingContract: LeasingContract;
 
+    brands: Brand[] = [];
+    models: Model[] = [];
+    tempmodels: Model[] = [];
 
   constructor(private leasingService: LeasingContractService,
               private route: ActivatedRoute,
-              private router: Router) {
+              private router: Router,
+              private brandService: BrandService,) {
     this.leasingContract = new LeasingContract();
   }
 
   ngOnInit(): void {
-    this.getLeasingContract(this.route.snapshot.params["id"])
+    this.getLeasingContract(this.route.snapshot.params["id"]);
+      this.getBrands();
+
   }
 
   getLeasingContract(id: string): void {
-    console.log("ID = " + id);
     this.leasingService.get(id)
         .subscribe({
           next: (data) => {
             this.leasingContract = data;
-            console.log("data = "+data);
-            console.log("firstName = "+ this.leasingContract.customer);
-
           },
           error: (e) => console.error(e)
         });
@@ -43,12 +48,59 @@ export class LeasingContractDetailsComponent implements OnInit {
     this.leasingService.update(this.leasingContract.id, this.leasingContract)
         .subscribe({
           next: (res) => {
-            console.log(res);
-            this.message = res.message ? res.message : 'This contract was updated'
+            this.message = res.message ? res.message : 'This Leasing Contract was updated!'
           },
           error: (e) => console.log(e)
         })
   }
+
+
+    getBrands() {
+        this.brandService.getAll()
+            .subscribe({
+                next: (res) => {
+                    this.brands = res;
+                    this.setSelectedModels(this.tempmodels);
+                },
+                error: (e) => console.log(e)
+            })
+    }
+
+    setSelectedModels(tempmodels: any) {
+        let vehicleBrand = this.leasingContract.vehicle?.brand;
+        this.brands.forEach(function(brand) {
+            if(vehicleBrand === brand.name) {
+                tempmodels = brand.models;
+            }
+        })
+        this.models = tempmodels;
+    }
+
+    selectBrand(target: any) {
+        this.models = this.brands[target.value-1].models;
+
+        console.log("booo " + this.brands[target.value-1].name)
+        console.log("tttt " + this.models[0].name)
+        if(this.leasingContract.vehicle) {
+            this.leasingContract.vehicle.brand = this.brands[target.value-1].name;
+            this.leasingContract.vehicle.model = this.models[0].name;
+            console.log("nje123123123e");
+
+        }
+    }
+
+    selectModel(target: any) {
+        let tempModel;
+        this.models.forEach(function(model) {
+            if(target.value === model.name) {
+                tempModel = model.name;
+            }
+        })
+        if(this.leasingContract.vehicle) {
+            this.leasingContract.vehicle.model = tempModel;
+            console.log("njee");
+        }
+    }
 
 
 
